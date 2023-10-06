b2d9978acedcc357955bde1069b3b24582bb7e10
ThiagoHBA <thiagohhab@gmail.com>
2023-10-06 11:29:03 -0300

    adding attribute total count to monitor all players vote in userDidActDTO

--- Sources/ClioEntities/DTOS/UserDidActDTO.swift
+++ Sources/ClioEntities/DTOS/UserDidActDTO.swift
@@ -9,8 +9,10 @@ import Foundation
 
 public struct UserDidActDTO: DTO {
     public let submitCount: Int
+    public let totalCount: Int
     
-    public init(submitCount: Int) {
+    public init(submitCount: Int, totalCount: Int) {
         self.submitCount = submitCount
+        self.totalCount = totalCount
     }
 }
