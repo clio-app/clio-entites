aaf1d435f02d9756e76c1939cdeffc34230652ab
ThiagoHBA <thiagohhab@gmail.com>
2023-10-19 09:53:39 -0300

    adding artefacts to user

--- Sources/ClioEntities/User.swift
+++ Sources/ClioEntities/User.swift
@@ -11,10 +11,12 @@ public struct User: Codable, Equatable {
     public let id: UUID
     public let name: String
     public let picture: String
+    public let artefact: SessionArtefacts?
     
-    public init(id: UUID, name: String, picture: String) {
+    public init(id: UUID, name: String, picture: String, artefacts: SessionArtefacts?) {
         self.id = id
         self.name = name
         self.picture = picture
+        self.artefact = artefacts
     }
 }
