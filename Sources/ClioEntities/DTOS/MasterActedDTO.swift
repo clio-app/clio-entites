fc4a5ef41a3ec03d4cb87b136cac9d433fcaa570
ThiagoHBA <thiagohhab@gmail.com>
2023-11-07 10:35:08 -0300

    modifing master act entitie

--- Sources/ClioEntities/DTOS/MasterActedDTO.swift
+++ Sources/ClioEntities/DTOS/MasterActedDTO.swift
@@ -8,13 +8,11 @@
 import Foundation
 
 public struct MasterActedDTO: DTO {
-    public let picture: Data
-    public let pictureBase64: String
+    public let pictureID: UUID
     public let description: String
     
-    public init(picture: Data, pictureBase64: String, description: String) {
-        self.picture = picture
-        self.pictureBase64 = pictureBase64
+    public init(pictureID: UUID, description: String) {
+        self.pictureID = pictureID
         self.description = description
     }
 }
